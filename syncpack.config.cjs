cb2f8912a17fbf8166362a4a51d5a209a7a353d9
SetDefaultOne <setdefaultone@gmail.com>
2024-11-26 20:18:17 +0600

    chore(global): setup package.json management
    Signed-off-by: SetDefaultOne <setdefaultone@gmail.com>

---
+++ syncpack.config.cjs
@@ -0,0 +1,27 @@
+/**
+ * Syncpack configuration.
+ * @see https://jamiemason.github.io/syncpack/config/syncpackrc/
+ * @type {import("syncpack").RcFile}
+ */
+const config = {
+    indent: "    ",
+    semverGroups: [
+        {
+            range: "",
+            packages: ["**"],
+            dependencies: ["**"],
+            label: "the version of npm dependencies must always be exact",
+        },
+    ],
+    versionGroups: [
+        {
+            dependencies: ["@types/**"],
+            dependencyTypes: ["!dev"],
+            label: "the @types/* dependencies must always be dev dependencies",
+            isBanned: true,
+        },
+    ],
+    source: ["package.json", "**/package.json", "!node_modules/"],
+};
+
+module.exports = config;
