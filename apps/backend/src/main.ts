415759abe6713df4958f8e3b1eb6b9a9de25b626
Default One <setdefaultone@gmail.com>
2025-01-06 06:06:11 +0000

    feat: use sdk library in applications

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,18 @@
+declare const module: any;
+
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { name } from "@d1stack/sdk";
 
 async function bootstrap() {
+    console.log("sdk name", { name });
+
     const app = await NestFactory.create(AppModule);
     await app.listen(process.env.PORT ?? 3000);
+
+    if (module.hot) {
+        module.hot.accept();
+        module.hot.dispose(() => app.close());
+    }
 }
 bootstrap();
